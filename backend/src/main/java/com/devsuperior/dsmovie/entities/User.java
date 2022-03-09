65a1ced408f25ae76a3a6186c8a31db6d2a5fe85
Amanda Reis <amandareismaria@gmail.com>
2022-03-09 20:13:16 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/User.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dsmovie.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+
+	public User() {
+	}
+
+	public User(Long id, String email) {
+		super();
+		this.id = id;
+		this.email = email;
+	}
+
+}
